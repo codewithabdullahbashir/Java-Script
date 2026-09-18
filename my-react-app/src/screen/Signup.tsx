878c4b8a7e7ea0584cf0dbb77651641dc6b092e3
Abdullah1Bashir <lahoreedufund201819@gmail.com>
2026-09-18 15:51:20 +0500

    Add Image

--- my-react-app/src/screen/Signup.tsx
+++ my-react-app/src/screen/Signup.tsx
@@ -7,8 +7,15 @@ const Signup = () => {
       <header className="mx-auto flex h-16 max-w-7xl items-center justify-end px-6">
         <img src={Logo} alt="App Logo" className="h-8 w-auto object-contain" />
       </header>
-
-      
+      <main className="mx-auto grid max-w-7xl grid-cols-1 gap-8 px-6 py-8 md:grid-cols-2">
+        <div className="flex justify-strat">
+          <img
+            src={SignupImg}
+            alt="Sign up illustration"
+            className="h-auto max-h-150 w-full max-w-md object-contain"
+          />
+        </div>
+      </main>
     </div>
   );
 };
