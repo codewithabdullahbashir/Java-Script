7d375ce92aebdd4519bd7d672c9b3b4998d70c94
Abdullah1Bashir <lahoreedufund201819@gmail.com>
2026-09-18 17:00:53 +0500

    Add Text fileds

--- my-react-app/src/screen/Signup.tsx
+++ my-react-app/src/screen/Signup.tsx
@@ -19,12 +19,84 @@ const Signup = () => {
           </div>
           <div className="w-full max-w-md">
             <form>
-              <h1 className="text-2xl font-semibold text-slate-900">Sign up</h1>
-
-              <p className="mt-1 text-[9px] text-gray-500">
+              <h1 className="text-[40px] font-semibold text-slate-900">
+                Sign up
+              </h1>
+              <p className="mt-1 text-[16px] text-gray-500">
                 Let's get you all set up so you can access your personal
                 account.
               </p>
+              <div className="mt-5 grid grid-cols-2 gap-3">
+                <div>
+                  <label className="mb-1 block text-[14px] text-gray-600">
+                    First Name
+                    <input
+                      type="text"
+                      placeholder="Full Name"
+                      className="h-9.75 w-full rounded-sm border border-gray-300 px-2 text-[16px] outline-none focus:border-indigo-500"
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label className="mb-1 block text-[14px] text-gray-600">
+                    Last Name
+                    <input
+                      type=""
+                      placeholder="Last Name"
+                      className="h-9.75 w-full rounded-sm border border-gray-300 px-2 text-[16px] outline-none focus:border-indigo-500"
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="mt-2 grid grid-cols-2 gap-2">
+                <div>
+                  <label className="mb-1 block text-[14px] text-gray-600">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="@gmail.com"
+                    className="h-9.75 w-full rounded-sm border border-gray-300 px-2 text-[16px] outline-none focus:border-indigo-500"
+                  />
+                </div>
+
+                <div>
+                  <label className="mb-1 block text-[14px] text-gray-600">
+                    Phone Number
+                  </label>
+                  <input
+                    type="tel"
+                    placeholder="+92 3000000000"
+                    className="h-9.75 w-full rounded-sm border border-gray-300 px-2 text-[16px] outline-none focus:border-indigo-500"
+                  />
+                </div>
+              </div>
+              <div className="mt-2">
+                <label className="mb-1 block text-[14px] text-gray-600">
+                  Password
+                </label>
+
+                <div className="relative">
+                  <input
+                    type="password"
+                    placeholder="**4******************f"
+                    className="h-9.75 w-full rounded-sm border border-gray-300 px-2 pr-8 text-[16px] outline-none focus:border-indigo-500"
+                  />
+                </div>
+              </div>
+              <div className="mt-2">
+                <label className="mb-1 block text-[14px] text-gray-600">
+                  Confirm Password
+                </label>
+
+                <div className="relative">
+                  <input
+                    type="password"
+                    placeholder="**4******************f"
+                    className="h-9.75 w-full rounded-sm border border-gray-300 px-2 pr-8 text-[16px] outline-none focus:border-indigo-500"
+                  />
+                </div>
+              </div>
             </form>
           </div>
         </div>
