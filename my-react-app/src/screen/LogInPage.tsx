a57cea75ac5d51db346ac313f5196f8468c2995e
Abdullah1Bashir <lahoreedufund201819@gmail.com>
2026-09-22 13:12:26 +0500

    Add Login in Page

--- my-react-app/src/screen/LogInPage.tsx
+++ my-react-app/src/screen/LogInPage.tsx
@@ -1,10 +1,14 @@
+import LoginLogo from "../assets/LoginLogo.png"
+
 const LogInPage = () => {
   return (
     <div className="grid grid-cols-2 h-screen w-full">
       <section className="h-full w-full bg-white">
-        <div></div>
+        <div className="flex text-center  ">
+          <img src={LoginLogo} alt="Login Logo" className="h-8 w-31" />
+        </div>
       </section>
-      <section className="h-full w-full bg-green-800"></section>
+      <section className="h-full w-full bg-[#1C4532]"></section>
     </div>
   );
 };
